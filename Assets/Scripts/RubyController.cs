125a4947994caaacf5e023d90c4e9fdc562e24ab
Jaimin Parmar <jaimins100@gmail.com>
2024-05-03 11:39:41 +0530

    add ruby controller

---
+++ Assets/Scripts/RubyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RubyController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 position = transform.position;
+        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
+        position.y = position.y + 3.0f * vertical * Time.deltaTime;
+        transform.position = position;
+    }
+}

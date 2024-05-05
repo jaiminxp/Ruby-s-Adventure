2bdfb55ada74f7f5d06b2d6236ddeaac947fdd25
Jaimin Parmar <jaimins100@gmail.com>
2024-05-05 10:38:58 +0530

    destroy projectile if it is far enough

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -11,6 +11,14 @@ public class Projectile : MonoBehaviour
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
+    void Update()
+    {
+        if (transform.position.magnitude > 1000.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public void Launch(Vector2 direction, float force)
     {
         rigidbody2D.AddForce(direction * force);

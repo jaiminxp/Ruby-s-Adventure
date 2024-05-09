7a4fce1884143360f57b6aa580f6507a38d32971
Jaimin Parmar <jaimins100@gmail.com>
2024-05-09 12:52:45 +0530

    display a dialog for jambi NPC

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -60,6 +60,22 @@ public class RubyController : MonoBehaviour
         {
             Launch();
         }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f,
+                lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+
+            if (hit.collider != null)
+            {
+                NPC character = hit.collider.GetComponent<NPC>();
+
+                if (character != null)
+                {
+                    character.DisplayDialog();
+                }
+            }
+        }
     }
 
     void FixedUpdate()
